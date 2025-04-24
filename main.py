ef0231607ea0d9c2eb5f4b0e0eda5e77171a7748
Paul Williamson <paulw.900430@gmail.com>
2025-04-24 22:20:27 +0100

    refactor: main

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ if os.path.exists("debug.log"):
 logging.basicConfig(level=logging.INFO, filename="debug.log")
 
 
-LLM = get_llm()
-CHAT_MEMORY = ChatMemoryBuffer(token_limit=40_000)
+def main():
+    LLM = get_llm()
+    CHAT_MEMORY = ChatMemoryBuffer(token_limit=40_000)
 
-async def run_agent(input: str, chat_history: ChatMemoryBuffer) -> ChatMemoryBuffer:
-    async with MCPAgent(llm=LLM, chat_history=chat_history) as agent:
-        agent = MCPAgent(llm=LLM, chat_history=chat_history)
-        await agent.run(input=input)
-        return agent.chat_history
-    
-user_input = "Hello, fetch any weather alerts for New Jersey."
+    async def run_agent(input: str, chat_history: ChatMemoryBuffer) -> ChatMemoryBuffer:
+        async with MCPAgent(llm=LLM, chat_history=chat_history) as agent:
+            agent = MCPAgent(llm=LLM, chat_history=chat_history)
+            await agent.run(input=input)
+            return agent.chat_history
+        
+    user_input = "Hello, fetch any weather alerts for New Jersey."
 
-chat_history = asyncio.run(run_agent(user_input, CHAT_MEMORY))
+    chat_history = asyncio.run(run_agent(user_input, CHAT_MEMORY))
 
-chat_history = chat_history.get_all()
+    chat_history = chat_history.get_all()
 
-for message in chat_history:
-    print(f"{message.role}: {message.content}")
+    for message in chat_history:
+        print(f"{message.role}: {message.content}")
+        
+
+if __name__ == "__main__":
+    main()
